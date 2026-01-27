4008a0a67bbd8a0fd1fe0391c8b287443eb0ae67
Harshal Patil <harshal1232413@gmail.com>
2026-01-27 09:13:26 +0530

    Updated the socket URL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,5 @@ import io from 'socket.io-client';
 import { BASE_URL } from './constants';
 
 export const createSocketConnection = () => {
-  return io("/",{ path: "/api/socket.io "});
+  return io("/",{ path: "/api/socket.io"});
 }
